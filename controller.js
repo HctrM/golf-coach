1321fbf822966f8023e466e071302e781d2d65fc
Hctr <toptotor@hotmail.fr>
2017-07-29 22:58:54 +0200

    Add files via upload
    New handler for navigation in app.
    Tweaking of app overlay.
    Working !

--- controller.js
+++ controller.js
@@ -1,43 +1,58 @@
 var trou = 1;
-var golfPerf ={score:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], fir:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], gir:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], putt:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]};
-var firCheck = false;
-var girCheck = false;
+const golfPerf ={score:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], fir:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], gir:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], putt:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]};
+let firCheck = false;
+let girCheck = false;
+const contentList = ["keypad","girfir","putt"]; //To be automatically created
+var cpt = 0;
+var menuExtended = false;
 
 $(document).ready(function(){
-    //Handle keys and transitions from keypad to regulation
         $(".key").click(function(){
             $(".key").css("background-color","darkgreen");
             $(this).css("background-color","green");
             $("#next").css("display","inline-block");
             golfPerf.score[trou-1] = parseInt($(this).html());
-            console.log(golfPerf.score);
-        })
-
-        $('body').on('click','#next',function(){
-            if (golfPerf.score[trou-1] == 0){
-                $("#putt").css("display","none");
-                $("#keypad").css("display","inline-block");
-            }
-            else{
-                // $("#keypad").toggle("slide", "left");
-                $("#keypad").css("display","none");
-                // $("#girfir").toggle("slide","right");
-                $("#girfir").css("display","block");
-                $("#girup").css("display","block");
-                $("#girdown").css("display","block");
-                $("#firup").css("display","block");
-                $("#firdown").css("display","block");
-                $("#next").css("display","none");
-            }
         })
     
-    //Handle thumbs in regulation
+    //Handle navigation and hole count 
+        $('body').on('click','#next',function(){ 
+            clearWindow();
+            cpt += 1;
+            if (cpt > contentList.length-1){
+                cpt = 0;
+                trou = trou + 1;
+                girCheck = false;
+                firCheck = false;
+                var hole = "Trou " + trou
+                $("#holeCount").html(hole);
+                if (trou == 19){
+                    results();
+                    return;
+                }
+            }
+            showNext();
+            $("#next").css("display","none");
+
+        })
+    
+    //Burger menu
         $('body').on('click','#menuButton',function(){
+            if (menuExtended == true){
+                $("#menuButton").css("left","10px");
+                menuExtended = false;
+            }
+            else{
+                $("#menuButton").css("left","50px");
+                menuExtended = true;
+            }
             $("#navbar").toggle("slide","left");
             $("#navbar").css("display","inline-block");
-            $("#menuButton").css("right","50px");
+           
+            
+           
         })
 
+    //Handle input on gir fir
         $('body').on('click','#gir img', function(){
             if ($(this)[0].id=="girup"){
                 $('#girdown').css("display","none");
@@ -49,6 +64,7 @@ $(document).ready(function(){
             girCheck = true;
             checkFunc();
         })
+
         $('body').on('click','#fir img', function(){
             if ($(this)[0].id=="firup"){
                 $('#firdown').css("display","none");
@@ -65,96 +81,66 @@ $(document).ready(function(){
         $('.puttKey').click(function(){
             $(".puttKey").css("background-color","darkgreen");
             $(this).css("background-color","green");
-            $("#nextPutt").css("display","inline-block");
+            $("#next").css("display","inline-block");
             golfPerf.putt[trou-1] = parseInt($(this).html());
         })
-        $('body').on('click','#nextPutt',function(){
-            // $("#keypad").toggle("slide", "left");
-            $("#girfir").css("display","none");
-            // $("#girfir").toggle("slide","right");
-            $("#putt").css("display","inline-block");
-            $(".puttKey").css("display","inline-block");
-            console.log(golfPerf.putt);
-            if (golfPerf.putt[trou-1] != 0){
-                $("#nextPutt").attr("id","next");
-                trou = trou + 1;
-                var hole = "Trou " + trou
-                $("#holeCount").html(hole);
-            }
-        })
-
-   
-   
-
-function checkFunc(){
-    if (firCheck == true && girCheck == true){
-        $("#next").css("display","block");
-        $("#next").attr("id","nextPutt"); 
-    }
-}
-
-    // Method with thumbs
-    // $('body').on('click','.imgUp', function(){
-    //     console.log($(this)[0].id);
-    //     if ($(this)[0].id == "girup"){
-    //         if ($("#girdown")[0].src.indexOf("img/thumbUpRDark.png") != -1){
-    //             $("#girdown").attr("src","img/thumbUpR.png");
-    //         }
-    //     }
-
-    //     if ($(this)[0].id == "firup"){
-    //         if ($("#firdown")[0].src.indexOf("img/thumbUpRDark.png") != -1){
-    //             $("#firdown").attr("src","img/thumbUpR.png");
-    //         }
-    //     }
-
-    //     if ($(this)[0].src.indexOf("/img/thumbUp.png") != -1){
-    //         $(this).attr("src","img/thumbUpLight.png");
-    //     }
-    //     else{
-    //         $(this).attr("src","img/thumbUp.png");
-    //     }
-        
-    // })
-
-    // $('body').on('click','.imgDown', function(){
-    //     console.log($(this)[0].src);
-    //     if ($(this)[0].src.indexOf("/img/thumbUpR.png") != -1){
-    //         $(this).attr("src","img/thumbUpRDark.png");
-    //     }
-    //     else{
-    //         $(this).attr("src","img/thumbUpR.png");
-    //     }
-
-    //     if ($(this)[0].id == "girup"){
-    //         if ($("#girup")[0].src.indexOf("img/thumbUpRDark.png") != -1){
-    //             $("#girup").attr("src","img/thumbUpR.png");
-    //         }
-    //     }
-
-    //     if ($(this)[0].id == "firup"){
-    //         if ($("#firup")[0].src.indexOf("img/thumbUpRDark.png") != -1){
-    //             $("#firup").attr("src","img/thumbUpR.png");
-    //         }
-    //     }
-        
-    // })
 })
 
 
-
-function score(val){
-    golfPerf.score[trou-1] = val;
-    if (trou == 19){
-        var cpt = 0;
-        var i = 0;
-        while (i <= 17){
-            cpt = cpt + golfPerf.score[i];
-            i = i + 1;
-        }
-        var score = cpt;
+function checkFunc(){
+    if (firCheck == true && girCheck == true){
+        $("#next").css("display","inline-block");
     }
 }
+
+function clearWindow(){
+    $("#" + contentList[cpt]).css("display","none");
+    $("#" + contentList[cpt] + " *").css("display","none");
+    // console.log("clear : " + cpt);
+}
+
+function showNext(){
+    // console.log("show : " + (cpt+1));
+    $("#" + contentList[cpt]).css("display","inline-block");
+    $("#" + contentList[cpt] + " *").css("display","inline-block");
+}
+
+function results(){
+    $("#results button").css("display","inline-block");
+    $("#scorecard").css("display","inline-block");
+    var table="<tr>";
+    table = table + "<td style=\"width:60px\">" + "Trou" + "</td>";
+    for (i = 0; i < 18;i++){
+        table = table + "<td>" + (i+1) + "</td>";
+    }
+    table = table + "</tr>"
+    table = table + "<tr>"
+    table = table + "<td style=\"width:60px\">" + "GIR" + "</td>";
+    for (i = 0; i < 18;i++){
+        table = table + "<td>" + golfPerf.gir[i] + "</td>";
+    }
+    table = table + "</tr>"
+    table = table + "<tr>"
+    table = table + "<td style=\"width:60px\">" + "FIR" + "</td>";
+    for (i = 0; i < 18;i++){
+        table = table + "<td>" + golfPerf.fir[i] + "</td>";
+    }
+    table = table + "</tr>"
+    table = table + "<tr>"
+    table = table + "<td style=\"width:60px\">" + "PUTT" + "</td>";
+    for (i = 0; i < 18;i++){
+        table = table + "<td>" + golfPerf.putt[i] + "</td>";
+    }
+    table = table + "</tr>"
+    table = table + "<tr>"
+    table = table + "<td style=\"width:60px\">" + "SCORE" + "</td>";
+    for (i = 0; i < 18;i++){
+        table = table + "<td>" + golfPerf.score[i] + "</td>";
+    }
+    table = table + "</tr>"
+    $("#scorecard").html(table);
+}
+
 
 
 
